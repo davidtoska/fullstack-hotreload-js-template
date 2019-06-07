05a2282dd2e38303ec19a842b11366466cbee99e
David toska <david.toska@outlook.com>
2019-06-07 20:58:18 +0200

    devserver and babel is working. and images is loadeing

---
+++ config/webpack.dev.js
@@ -0,0 +1,56 @@
+const path = require("path");
+module.exports = {
+  entry: {
+    main: ["./src/main.js"]
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "dist",
+    overlay: true
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: [{ loader: "babel-loader" }], exclude: /node_modules/ },
+      {
+        test: /\.css/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].html"
+            }
+          },
+          // makes the html a separate file
+          { loader: "extract-loader" },
+          // Linting the html
+          {
+            loader: "html-loader",
+            options: {
+              attrs: ["img:src"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|gif|jpeg|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name]-[hash:8].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
---
+++ src/main.js
@@ -0,0 +1,13 @@
+// @ts-ignore
+require("./main.css");
+// @ts-ignore
+require("./index.html");
+console.log("hello world 2asdf ");
+console.log("yalla yalla");
+
+const a = async () => {
+  await console.log("hello from the future");
+  console.log("Done");
+};
+
+a();
